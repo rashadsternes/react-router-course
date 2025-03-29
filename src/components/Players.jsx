e4f21a5d35459b5aec9971debbd430fa4bba3069
rashadsternes <nrsternes@gmail.com>
2025-03-28 20:48:20 -0500

    Add Player Functionality

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import { Link, Outlet, useLocation, useSearchParams } from 'react-router-dom';
 import usePlayerNames from '../hooks/usePlayerNames';
 import { slugify } from '../utils';
 
@@ -64,8 +64,9 @@ export default function Players () {
     }
 
     return (
-        <div className='container'>
+        <div className='container two-column'>
             <Sidebar title='Players' list={names} />
+            <Outlet />
         </div>
     );
 }
