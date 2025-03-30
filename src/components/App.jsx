120e294a02e1471c762a4631b08b9a9b182071a2
rashadsternes <nrsternes@gmail.com>
2025-03-30 04:01:04 -0500

    Add code-split functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import {
   BrowserRouter as Router,
   Routes,
@@ -5,58 +6,60 @@ import {
 } from 'react-router-dom';
 
 import Navbar from './Navbar';
-import Home from './Home';
-import Teams from './Teams';
-import Players from './Players';
-import TeamPage from './TeamPage';
-import Player from './Player';
-import Team from './Team';
-import Articles from './Articles';
-import Article from './Article';
+import Loading from './Loading';
+const Home = React.lazy(() => import('./Home'));
+const Teams = React.lazy(() => import('./Teams'));
+const Players = React.lazy(() => import('./Players'));
+const TeamPage = React.lazy(() => import('./TeamPage'));
+const Player = React.lazy(() => import('./Player'));
+const Team = React.lazy(() => import('./Team'));
+const Articles = React.lazy(() => import('./Articles'));
+const Article = React.lazy(() => import('./Article'));
 
 export default function App () {
   return (
     <Router>
       <div>
         <Navbar />
-
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/players' element={<Players />}> 
-            <Route path=':playerId' element={<Player />} />
-            <Route 
-              path=''
-              element={
-                <div className='sidebar-instruction'>
-                  Select a Player
-                </div>
-              }
-            />
-          </Route>
-          <Route path='/teams' element={<Teams />} >
-            <Route path=':teamId' element={<Team />} />
-            <Route 
-              path=''
-              element={
-                <div className='sidebar-instruction'>
-                  Select a Team
-                </div>
-              }
-            />
-          </Route>
-          <Route path='/:teamId' element={<TeamPage />} />
-          <Route path='/:teamId/articles' element={<Articles />} >
-            <Route path=':articleId' element={<Article />} />
-            <Route 
-              path=''
-              element={
-                <div className='sidebar-instruction'>
-                  Select an Article
-                </div>
-              }
-            />
-          </Route>
-        </Routes>
+          <React.Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/players' element={<Players />}> 
+                <Route path=':playerId' element={<Player />} />
+                <Route 
+                  path=''
+                  element={
+                    <div className='sidebar-instruction'>
+                      Select a Player
+                    </div>
+                  }
+                />
+              </Route>
+              <Route path='/teams' element={<Teams />} >
+                <Route path=':teamId' element={<Team />} />
+                <Route 
+                  path=''
+                  element={
+                    <div className='sidebar-instruction'>
+                      Select a Team
+                    </div>
+                  }
+                />
+              </Route>
+              <Route path='/:teamId' element={<TeamPage />} />
+              <Route path='/:teamId/articles' element={<Articles />} >
+                <Route path=':articleId' element={<Article />} />
+                <Route 
+                  path=''
+                  element={
+                    <div className='sidebar-instruction'>
+                      Select an Article
+                    </div>
+                  }
+                />
+              </Route>
+            </Routes>
+          </React.Suspense>
       </div>
     </Router>
   );
